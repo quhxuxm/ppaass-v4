8ecd551533622e7a31a87e49c5e11e402eaf68b9
Qu Hao <quhxuxm_quh@163.com>
2025-08-07 20:38:12 +0800

    change

--- common/src/pool.rs
+++ common/src/pool.rs
@@ -3,7 +3,7 @@ use crate::user::UserWithProxyServers;
 use crate::WithConnectionPoolConfig;
 use std::sync::{Arc, OnceLock};
 use std::time::Duration;
-use tokio::sync::{Mutex, Semaphore};
+use tokio::sync::Mutex;
 use tokio::time::sleep;
 use tracing::error;
 
@@ -14,7 +14,6 @@ where
     'a: 'static,
 {
     connections: Arc<Mutex<Vec<ProxyConnection<ProxyFramed<'a>>>>>,
-    connections_semaphore: Arc<Semaphore>,
 }
 
 impl<'a> ProxyConnectionPool<'a>
@@ -29,13 +28,13 @@ where
         let proxy_connect_timeout = config.proxy_connect_timeout();
         let connection_pool_size = config.connection_pool_size();
         let connections = Arc::new(Mutex::new(vec![]));
-        let connections_semaphore = Arc::new(Semaphore::new(0));
         {
             let connections = connections.clone();
-            let connections_semaphore = connections_semaphore.clone();
             tokio::spawn(async move {
                 loop {
-                    if connections_semaphore.available_permits() >= connection_pool_size {
+                    let mut connections = connections.lock().await;
+                    if connections.len() >= connection_pool_size {
+                        drop(connections);
                         sleep(Duration::from_secs(1)).await;
                         continue;
                     }
@@ -46,22 +45,16 @@ where
                             return;
                         }
                     };
-                    connections.lock().await.push(proxy_connection);
-                    connections_semaphore.add_permits(1);
+                    connections.push(proxy_connection);
                 }
             });
         }
         ProxyConnectionPool {
-            connections,
-            connections_semaphore,
+            connections
         }
     }
     pub async fn fetch_connection(&self) -> ProxyConnection<ProxyFramed<'a>> {
         loop {
-            if let Err(e) = self.connections_semaphore.acquire().await {
-                error!("Fail to acquire proxy connection pool semaphore: {e:?}");
-                continue;
-            };
             let connection = self.connections.lock().await.pop();
             match connection {
                 None => {
