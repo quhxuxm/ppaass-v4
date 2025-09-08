26cf30b2613dbff48fb4cb3e43f3a59ea63d18dc
Qu Hao <quhxuxm_quh@163.com>
2025-09-08 21:39:43 +0800

    change - remove pool as it is not stable

--- common/src/log.rs
+++ common/src/log.rs
@@ -1,15 +1,18 @@
 use crate::{Error, ServerConfig};
-use std::str::FromStr;
-use tracing::Level;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::fmt::time::ChronoUtc;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
 
 pub fn init_log<C: ServerConfig>(config: &C) -> Result<WorkerGuard, Error> {
+    let tracing_subscriber_registry = tracing_subscriber::registry();
     let (trace_file_appender, trace_appender_guard) = tracing_appender::non_blocking(
         tracing_appender::rolling::daily(config.log_directory(), config.log_name_prefix()),
     );
-    tracing_subscriber::fmt()
-        .with_max_level(Level::from_str(config.max_log_level())?)
+    tracing_subscriber_registry.with(EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        EnvFilter::new(config.max_log_level())
+    })).with(tracing_subscriber::fmt::layer()
         .with_writer(trace_file_appender)
         .with_line_number(true)
         .with_level(true)
@@ -17,6 +20,6 @@ pub fn init_log<C: ServerConfig>(config: &C) -> Result<WorkerGuard, Error> {
         .with_thread_names(true)
         .with_timer(ChronoUtc::rfc_3339())
         .with_ansi(false)
-        .init();
+    ).init();
     Ok(trace_appender_guard)
 }
